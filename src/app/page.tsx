13213efa67b82b1652c8eada34b5111a9efade66
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-05-04 23:46:56 -0400

    New Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { Button } from '@/components/Button';
-
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-[72px] tracking-tighter font-black">
-        Hello React + Next
-      </h1>
-
-      <Button>Button</Button>
-    </main>
-  );
+  return <main>Olá Mundo!</main>;
 }
